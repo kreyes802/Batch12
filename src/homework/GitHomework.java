061b611164e74f3bf3c71817b597784644a1e77b
kreye <kreye@DESKTOP-0C08MK4.lan>
2024-09-12 22:00:17 -0400

    Made changes

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,25 +2,5 @@ package homework;
 
 public class GitHomework {
 	
-	public static void main (String[] args) {
-		System.out.println("Batch 12: Git Class Lecture");
-		int age = 12;
-		String name = "John";
-		//This is coming from github
-		
-	}
 	
-	public static void add(int a, int b) {
-		System.out.println(a + b);
-		//This is from github
-	}
-	
-	public static void subtract(int c, int d) {
-		System.out.println(c - d);
-	}
-	
-	public static void add1(int f, int g) {
-		System.out.println(f + g);
-	}
-
 }
