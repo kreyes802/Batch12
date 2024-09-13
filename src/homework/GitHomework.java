44b2bf7fe049282989fa9472ba5b020d3b315145
kreye <kreye@DESKTOP-0C08MK4.lan>
2024-09-12 21:18:54 -0400

    added new method

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -10,5 +10,9 @@ public class GitHomework {
 	public static void add(int a, int b) {
 		System.out.println(a + b);
 	}
+	
+	public static void subtract(int c, int d) {
+		System.out.println(c - d);
+	}
 
 }
