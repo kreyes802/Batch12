d1a985e37c1cd38748043de3615391e7a4db7fc0
kreyes802 <kreyes802@outlook.com>
2024-09-12 21:31:11 -0400

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -4,6 +4,9 @@ public class GitHomework {
 	
 	public static void main (String[] args) {
 		System.out.println("Batch 12: Git Class Lecture");
+		int age = 12;
+		String name = "John";
+		//This is coming from github
 		
 	}
 	
